3bd74497c4c98bc78baa06c45537f282722a5a0c
Mehemmai Mohammed Ridha <medridhameh@gmail.com>
2019-05-07 13:44:19 +0100

    External email management done (i guess)

--- IT Manager ENCC/Forms/Pass/externalEmailAdd.cs
+++ IT Manager ENCC/Forms/Pass/externalEmailAdd.cs
@@ -3,6 +3,7 @@ using System;
 using System.Data;
 using System.Windows.Forms;
 using static IT_Manager_ENCC.Modules.Tools;
+using static IT_Manager_ENCC.Modules.MYSQLConnect;
 
 
 namespace IT_Manager_ENCC.Forms.Pass
@@ -10,28 +11,58 @@ namespace IT_Manager_ENCC.Forms.Pass
     public partial class externalEmailAdd : DevExpress.XtraEditors.XtraForm
     {
         private externalEmailList externalEmailList;
+        private int focussedEmailId;
 
-        public externalEmailAdd(externalEmailList externalEmailList)
+        public externalEmailAdd(externalEmailList externalEmailList, int focussedEmailId=-1)
         {
             this.externalEmailList = externalEmailList;
+            this.focussedEmailId = focussedEmailId;
             InitializeComponent();
+
+            if(focussedEmailId != -1) {
+                confirmBtn.Text = "Edit Email";
+                emailTxt.Text = externalEmailList.emails.Rows[externalEmailList.getGridView().GetSelectedRows()[0]][1].ToString();
+                passwordTxt.Text = externalEmailList.emails.Rows[externalEmailList.getGridView().GetSelectedRows()[0]][2].ToString();
+            }
         }
 
         void addEmail()
         {
-            if (MYSQLConnect.IsConnected(MYSQLConnect.addSlash("Insert into externalemails VALUES (NULL, '" +
+            if (IsConnected(addSlash("Insert into externalemails VALUES (NULL, '" +
                 emailTxt.Text + "','" +
                 passwordTxt.Text + "',NOW())"), true))
             {
                 passwordTxt.Text = "";
                 emailTxt.Text = "";
-                externalEmailList.LoadEmails();
+                externalEmailList.LoadEmails(); //refresh email list
                 MessageBox.Show("email added successfully");
             }
             else
             {
                 MessageBox.Show("an error has occured during insertion");
             }
+        }
+
+
+        private void editEmail(string emailId)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to edit ?", "Edit confirmation message", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (IsConnected(addSlash("UPDATE externalemails SET" +
+                " email='" + emailTxt.Text + "'," +
+                " password='" + passwordTxt.Text + "'" +
+                " WHERE externalemails.id = '" + emailId + "' Limit 1;"), true))
+                {
+                    externalEmailList.LoadEmails(); //refresh email list
+                    MessageBox.Show("email updated successfully");
+                }
+                else
+                {
+                    MessageBox.Show("an error has occured during update");
+                }
+            }
+
         }
 
 
@@ -58,9 +89,11 @@ namespace IT_Manager_ENCC.Forms.Pass
             {
                 MessageBox.Show("Email invalid");
             }
+            else if (focussedEmailId == -1) // is edit email form ?
+                addEmail();
             else
             {
-                addEmail();
+                editEmail(focussedEmailId.ToString());
             }
         }
     }
--- IT Manager ENCC/Forms/Pass/externalEmailList.cs
+++ IT Manager ENCC/Forms/Pass/externalEmailList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Windows.Forms;
 using static IT_Manager_ENCC.Modules.MYSQLConnect;
 using static IT_Manager_ENCC.Modules.UiTools;
 
@@ -9,7 +10,7 @@ namespace IT_Manager_ENCC.Forms.Pass
     {
         private externalEmailAdd externalEmailAddForm;
         public MainForm mainForm;
-        private DataTable emails = new DataTable();
+        public DataTable emails = new DataTable();
 
         public externalEmailList(MainForm mainForm)
         {
@@ -21,8 +22,12 @@ namespace IT_Manager_ENCC.Forms.Pass
             emails.Columns.Add("Password");
             emails.Columns.Add("Creation date");
 
-
             LoadEmails();
+        }
+
+        public DevExpress.XtraGrid.Views.Grid.GridView getGridView()
+        {
+            return gridView1;
         }
 
         public void LoadEmails()
@@ -30,8 +35,18 @@ namespace IT_Manager_ENCC.Forms.Pass
             emails.Rows.Clear();
 
             if (IsConnected("SELECT * FROM `externalemails` ", false))
-                FillEmailDataGrid(myDR, emails, hardwareGrid, mainForm);
+                FillEmailDataGrid(myDR, emails, emailGrid, mainForm);
         }
+        private void RemoveHardware(string hardwareId)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete ?", "Remove confirmation message", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                IsConnected("DELETE FROM `externalemails` WHERE Id = '" + addSlash(hardwareId) + "' LIMIT 1;", true);
+                LoadEmails();
+            }
+        }
+
 
         private void addEmailBtn_Click(object sender, EventArgs e)
         {
@@ -39,5 +54,30 @@ namespace IT_Manager_ENCC.Forms.Pass
             externalEmailAddForm.Show();
         }
 
+        private void gridView1_RowClick(object sender, DevExpress.XtraGrid.Views.Grid.RowClickEventArgs e)
+        {
+            emailGrid.ContextMenuStrip = emailsMenu;
+        }
+
+        private void gridView1_LostFocus(object sender, EventArgs e)
+        {
+            emailGrid.ContextMenuStrip = null;
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            externalEmailAddForm = new externalEmailAdd(this, int.Parse(gridView1.GetFocusedRowCellValue("Id").ToString()));
+            externalEmailAddForm.Show();
+        }
+
+        private void deleateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RemoveHardware(gridView1.GetFocusedRowCellValue("Id").ToString());
+        }
+
+        private void searchTxt_EditValueChanged(object sender, EventArgs e)
+        {
+            gridView1.FindFilterText = searchTxt.Text;
+        }
     }
 }
